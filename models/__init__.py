8c0f94b0d93e29f6beeea1e054eb824f97e305a0
vanishedzhou <vanishedzhou@163.com>
2026-03-24 11:29:23 +0800

    init

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+import sqlite3
+import config
+
+
+def get_db():
+    """Get a database connection with row factory."""
+    conn = sqlite3.connect(config.DATABASE_PATH)
+    conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA journal_mode=WAL")
+    conn.execute("PRAGMA foreign_keys=ON")
+    return conn
